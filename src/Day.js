50cfa4f3d2110bb6dd36d03ee50b3346a31b16c6
Adrian Baertschi <adrian.baertschi@zuehlke.com>
2019-12-04 12:12:59 +0100

    Disable days in the future

--- src/Day.js
+++ src/Day.js
@@ -4,25 +4,30 @@ import './Day.css'
 export class Day extends React.Component {
     constructor(props, context) {
         super(props, context);
-        this.state = {isRevealed: false};
+        this.state = {
+            isRevealed: false,
+            isEnabled: new Date() > this.props.day
+        };
 
         this.reveal = this.reveal.bind(this);
     }
 
     reveal() {
-        this.setState({isRevealed: true})
+        if (this.state.isEnabled) {
+            this.setState({isRevealed: true})
+        }
     }
 
     render() {
         let content;
         if (this.state.isRevealed) {
-            const url = `https://source.unsplash.com/random/200x200?christmas${this.props.day.getDate()}`;
+            const url = `https://source.unsplash.com/random/200x200?christmas${this.props.day.getDate()}`; // add number to get a new image every time
             content = <img src={url} alt="pic"/>
         } else {
-            content = <a> {this.props.day.getDate()} </a>
+            content = <a className={this.state.isEnabled ? 'enabled' : 'disabled'}> {this.props.day.getDate()} </a>
         }
 
-        return <li className={this.state.isRevealed ? 'open' : 'closed'} onClick={this.reveal}>
+        return <li className="day" onClick={this.reveal}>
             {content}
         </li>;
     }
