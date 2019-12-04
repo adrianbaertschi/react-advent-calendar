90caea8916392926eda6989552d67e0475c90f6c
Adrian Baertschi <adrian.baertschi@zuehlke.com>
2019-12-04 11:39:23 +0100

    Add images

--- src/Day.js
+++ src/Day.js
@@ -2,7 +2,28 @@ import React from "react";
 import './Day.css'
 
 export class Day extends React.Component {
+    constructor(props, context) {
+        super(props, context);
+        this.state = {isRevealed: false};
+
+        this.reveal = this.reveal.bind(this);
+    }
+
+    reveal() {
+        this.setState({isRevealed: true})
+    }
+
     render() {
-        return <li className="day">{this.props.day.getDate()}</li>;
+        let content;
+        if (this.state.isRevealed) {
+            const url = `https://source.unsplash.com/random/200x200?christmas${this.props.day.getDate()}`;
+            content = <img src={url} alt="pic"/>
+        } else {
+            content = <a> {this.props.day.getDate()} </a>
+        }
+
+        return <li className={this.state.isRevealed ? 'open' : 'closed'} onClick={this.reveal}>
+            {content}
+        </li>;
     }
 }
